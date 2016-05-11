a99fae7cf01b8f993241df12ee7e1b8aea9e83d4
parth desai <psdesai.93@gmail.com>
2016-05-11 15:06:27 -0400

    display details of batsmen and addcss

--- app/components/BatsmenDetail.js
+++ app/components/BatsmenDetail.js
@@ -3,14 +3,23 @@ var React = require('react');
 var BatsmenDetailStyles = {
   batsmenList: {
     marginTop: "30px"
+  },
+  detailItem: {
+    marginLeft: "0px"
   }
 }
 
 
 function Batsmen (props){
   return(
-    <div>
-      {props.batsmen.name_display_first_last}
+    <div className= "row" style={BatsmenDetailStyles.detailItem}>
+    <span className= "col-sm-2" style={BatsmenDetailStyles.detailItem}>  {props.batsmen.name_display_first_last} </span>
+    <span className= "col-sm-1" style={BatsmenDetailStyles.detailItem}> {props.batsmen.a} </span>
+    <span className= "col-sm-1" style={BatsmenDetailStyles.detailItem}>   {props.batsmen.h} </span>
+    <span className= "col-sm-1" style={BatsmenDetailStyles.detailItem}>   {props.batsmen.rbi} </span>
+    <span className= "col-sm-1" style={BatsmenDetailStyles.detailItem}>   {props.batsmen.bb}  </span>
+    <span className= "col-sm-1" style={BatsmenDetailStyles.detailItem}>   {props.batsmen.so}  </span>
+    <span className= "col-sm-1" style={BatsmenDetailStyles.detailItem}>   {props.batsmen.avg} </span>
     </div>
   )
 }
