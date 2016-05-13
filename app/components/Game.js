be065d02eb0da4ebb5f6fb0195a9260b70ce59fe
parth desai <psdesai.93@gmail.com>
2016-05-13 18:12:37 -0400

    chech for winner, apply bold class to winner!

--- app/components/Game.js
+++ app/components/Game.js
@@ -1,20 +1,19 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var DatePicker = require('react-datepicker');
+var moment = require('moment');
 
 
 //    background-color: #eeeeee;
 // box-shadow: 1px 1px 1px 1px;
 var GameStyles = {
   homeTeam: {
-    fontWeight : "bold",
     fontSize: "20px",
     marginTop: "10px",
     fontFamily: "'Roboto', sans-serif"
   },
   awayTeam: {
     fontSize:"20px",
-    fontWeight: "lighter",
     fontFamily: "'Roboto', sans-serif"
 
   },
@@ -52,12 +51,29 @@ var GameStyles = {
     marginTop : "15px",
     marginBottom: "15px",
     color: "#b3b3b3"
+  },
+  dateWrapper: {
+    marginTop: "15px",
+    textAlign: "center",
+    fontSize: "20px"
+  },
+  winner: {
+    fontWeight: "bold"
+  },
+  loser: {
+    fontWeight: "lighter"
   }
+}
+
+var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+function checkWinner(data){
+
 }
 
 function FavFilter(props){
   return(
-    <div className=" col-xs-5 col-sm-4 col-sm-offset-2" style={GameStyles.favFilterWrapper}>
+    <div className=" col-xs-4 col-sm-3 col-xs-offset-1" style={GameStyles.favFilterWrapper}>
       <div className="pull-right">
         <form className="form-inline"onSubmit={props.onSubmitFav}>
           <div className="form-group" data-toggle="tooltip" data-placement="bottom" title="Please enter the name of the team without city name. E.g: For Toronto Blue Jays, just enter Blue Jays">
@@ -80,17 +96,29 @@ function FavFilter(props){
 }
 
 function GameItem(props){
+  if(props.data.status.status === "Final"){
+    props.data.linescore.r.home > props.data.linescore.r.away
+      ? home = _.merge({},GameStyles.homeTeam, GameStyles.winner)
+      : home = _.merge({},GameStyles.homeTeam, GameStyles.loser)
+
+    props.data.linescore.r.away > props.data.linescore.r.home
+      ? away = _.merge({},GameStyles.awayTeam, GameStyles.winner)
+      : away = _.merge({},GameStyles.awayTeam, GameStyles.loser)
+  } else {
+    home = GameStyles.homeTeam
+    away = GameStyles.awayTeam
+  }
   return(
     <div className="row" onClick={props.handleClick} style={GameStyles.gameItemWrapper}>
     {
-      props.isEmpty
+      props.isEmpty === "true"
       ?
         <div className="col-md-7 col-md-offset-2" style={GameStyles.gamesNotFound}>
           <h1> {props.data} </h1>
         </div>
       :
         <div className="col-md-7 col-md-offset-2">
-            <div style={GameStyles.homeTeam}>
+            <div style={home}>
               {props.data.home_team_name}
               {
                 props.data.status.status === "Final"
@@ -98,7 +126,7 @@ function GameItem(props){
                  : null
               }
             </div>
-            <div style={GameStyles.awayTeam}>
+            <div style={away}>
               {props.data.away_team_name}
 
               {
@@ -115,24 +143,49 @@ function GameItem(props){
     </div>
   );
 }
+function GameItemEmpty(props){
+  return(
+    <div style={GameStyles.gamesNotFound}>
+      Looks like there were no games today
+    </div>
+  )
+}
 
 function GameUI(props){
-  return(
+   return(
     <div className="col-xs-10 col-sm-offset-1 col-xs-offset-1" style={GameStyles.gameWrapper}>
       {
-          props.isGameArray
-          ? props.data.games.game.map( function(info){
-            if(info.status.status.toLowerCase() === "final"){
-              return <GameItem key={info.id} data={info} handleClick={props.handleClick.bind(null,info)} isEmpty={props.isEmpty}/>
-            } else {
-              return <GameItem key={info.id} data={info} handleClick={null} isEmpty={props.isEmpty}/>
-            }})
-          : props.isEmpty === "true"
-            ? <GameItem key={props.data.games.game.id} data={props.data.games.game} handleClick={props.handleClick.bind(null,props.data.games.game)} isEmpty={props.isEmpty} />
-            : <GameItem key={null} data={"Sorry, no games were found for this day"} isEmpty={props.isEmpty} />
+          props.isEmpty
+          ? <GameItemEmpty  data={"Sorry, no games were found for this day"} isEmpty={props.isEmpty} />
+          : props.isGameArray
+            ? props.data.games.game.map( function(info){
+              if(info.status.status.toLowerCase() === "final"){
+                return <GameItem key={info.id} data={info} handleClick={props.handleClick.bind(null,info)} isEmpty={props.isEmpty}/>
+              } else {
+                return <GameItem key={info.id} data={info} handleClick={null} isEmpty={props.isEmpty}/>
+              }})
+          :props.isEmpty
+            ? <GameItem key={1} data={"Sorry, no games were found for this day"} isEmpty={props.isEmpty} />
+            : <GameItem key={props.data.games.game.id} data={props.data.games.game} handleClick={props.handleClick.bind(null,props.data.games.game)} isEmpty={props.isEmpty} />
       }
     </div>
   );
+  // return(
+  //   <div className="container " style={GameStyles.gameWrapper}>
+  //     {
+  //         props.isGameArray
+  //         ? props.data.games.game.map( function(info){
+  //           if(info.status.status.toLowerCase() === "final"){
+  //             return <GameItem key={info.id} data={info} handleClick={props.handleClick.bind(null,info)} isEmpty={props.isEmpty}/>
+  //           } else {
+  //             return <GameItem key={info.id} data={info} handleClick={null} isEmpty={props.isEmpty}/>
+  //           }})
+  //         : props.isEmpty === "true"
+  //           ?  <GameItem key={null} data={"Sorry, no games were found for this day"} isEmpty={props.isEmpty} />
+  //           : <GameItem key={props.data.games.game.id} data={props.data.games.game} handleClick={props.handleClick.bind(null,props.data.games.game)}  isEmpty={props.isEmpty}/>
+  //     }
+  //   </div>
+  // );
 }
 
 function Game(props) {
@@ -143,17 +196,20 @@ function Game(props) {
           ? <h1>Loading</h1>
           : <div className="container">
               <div className ="row">
-                <div className ="col-xs-5 col-sm-4 col-xs-offset-1 " style={GameStyles.datePickerWrapper}>
+                <div className ="col-xs-3 col-sm-3 col-xs-offset-1 " style={GameStyles.datePickerWrapper}>
                   <DatePicker selected={props.day}
                     onChange={props.onChange}
                     todayButton={'Today'}
                     className='form-control'
                     />
                 </div>
-                  <FavFilter onUpdateFavTeam={props.onUpdateFavTeam} favTeam={props.favTeam} onSubmitFav={props.onSubmitFav}/>
+                <div className="col-xs-3" style={GameStyles.dateWrapper}>
+                   {month[props.date.month()]} {props.date.date()}, {props.date.year()}
+                </div>
+                <FavFilter onUpdateFavTeam={props.onUpdateFavTeam} favTeam={props.favTeam} onSubmitFav={props.onSubmitFav}/>
               </div>
               <div className="row">
-                <GameUI data={props.finalData}  handleClick={props.handleClick} isGameArray={props.isGameArray} isEmpty={props.isEmpty} />
+                <GameUI data={props.finalData}  handleClick={props.handleClick} isGameArray={props.isGameArray} isEmpty={props.isEmpty} isLoading={props.isLoading}/>
               </div>
             </div>
         }
