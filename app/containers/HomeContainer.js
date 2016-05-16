612e8306332859655ce99bd9eb68554ce16e8c76
parth desai <psdesai.93@gmail.com>
2016-05-16 18:08:23 -0400

    refactor

--- app/containers/HomeContainer.js
+++ app/containers/HomeContainer.js
@@ -2,23 +2,9 @@ var React = require('react');
 var Home = require('../components/Home');
 
 var HomeContainer = React.createClass({
-  getInitialState: function() {
-    return {
-      date : ""
-    };
-  },
-  componentDidMount: function() {
-      var today = new Date();
-      var dd = today.getDate();
-      var mm = today.getMonth()+1;
-      console.log("dd : " + dd + "mm : " + mm);
-      this.setState({
-        date: today
-      })
-  },
   render: function() {
     return(
-      <Home fav="blue jays"/>
+      <Home />
     );
   }
 });
